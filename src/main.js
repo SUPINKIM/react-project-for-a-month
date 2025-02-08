4354a4857faebbc70d64b018a373c7d2b760ea67
SUPINKIM <qorgkr26@sookmyung.ac.kr>
2025-02-08 16:14:14 +0900

    refactor: readme 업데이트 및 불필요 css 파일 제거

--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
-import './style.css';
-
 import { Button } from './components/button.jsx';
 import App from './App';
 import { serializeVirtualDom } from './utils/serialize';
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello World!</h1>
-  </div>
-`;
 
 //document.querySelector('#app').appendChild(Button());
 
