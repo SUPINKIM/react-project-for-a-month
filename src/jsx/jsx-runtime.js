f56b93dec16e587d394d3aa6d75be26b9a642c13
SUPINKIM <qorgkr26@sookmyung.ac.kr>
2025-02-08 16:04:00 +0900

    feat: Fragment 타입 처리 추가

--- src/jsx/jsx-runtime.js
+++ src/jsx/jsx-runtime.js
@@ -2,3 +2,5 @@ import { createElement } from '@/utils/createElement';
 
 export const h = (component, props, ...children) =>
   createElement(component, props, children);
+
+export const Fragment = 'Fragment';
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
-import babel from 'vite-plugin-babel';
+//import babel from 'vite-plugin-babel';
 import viteJsconfigPaths from 'vite-jsconfig-paths';
 
 export default defineConfig({
   plugins: [viteJsconfigPaths()],
   esbuild: {
     jsx: 'transform',
-    jsxInject: 'import { h } from "@/jsx/jsx-runtime"',
+    jsxInject: 'import { h, Fragment } from "@/jsx/jsx-runtime"',
     jsxFactory: 'h',
+    jsxFragment: 'Fragment',
   },
 });
