e91803619ac92ccdf867068e0b71b411451aec43
SUPINKIM <qorgkr26@sookmyung.ac.kr>
2025-02-18 18:40:38 +0900

    chore: 주석 수정

--- src/components/todo-list.jsx
+++ src/components/todo-list.jsx
@@ -71,10 +71,14 @@ const TodoList = () => {
 
   return (
     <div>
-      {/* 부분 리렌더링으로 기존에 등록해 둔 DOM이 제거되지 않아야 
-      원하는 대로 submit 시점에 업데이트 된 todoInputValue 값을 가져올 수 있을 것이라 생각함
-      현재는 onChange 핸들러가 실행되면서 전체 DOM이 다시 그려지면서 기존 form 요소가 제거되면서 event listener가 제거됨
-      그래서 onChange 이벤트만 동작하고 onSubmit 이벤트가 동작하지 않는 문제가 발생 */}
+      {/* 
+        부분 리렌더링으로 input value는 계속해서 업데이트 되고 input value와 관계 없는 
+        form tag는 기존 dom이 재사용되어야 원하는 대로 submit 시점에 업데이트 된 
+        todoInputValue 값이 가져올 수 있을 것이라 생각함.
+        현재는 onChange 핸들러가 실행되면서 전체 DOM이 다시 그려지면서 
+        기존 form 요소가 제거되면서 event listener에 등록된 onSubmit 이벤트가 실행되지 않음
+        그래서 onChange 이벤트만 동작하고 onSubmit 이벤트가 동작하지 않는 문제가 발생
+      */}
 
       <form onSubmit={handleSubmit}>
         <input
