56ab38fb87dee308e74cbdfdaaac3791f8f3c2d6
SUPINKIM <qorgkr26@sookmyung.ac.kr>
2025-02-22 23:59:58 +0900

    feat: useEffect 구현 및 타이머 실행

--- src/utils/state.js
+++ src/utils/state.js
@@ -4,7 +4,11 @@ export default (function () {
   let index = 0;
   const state = [];
 
-  const forceUpdate = () => {
+  let prevDepsIndex = 0;
+  const prevDeps = [];
+  const clears = [];
+
+  const forceUpdate = async () => {
     index = 0; // index 초기화
     createRoot();
   };
@@ -27,5 +31,42 @@ export default (function () {
     return [state[_index], setState];
   };
 
-  return { useState };
+  const useEffect = (callback, deps) => {
+    const _index = prevDepsIndex;
+
+    clears.forEach((clear, idx) => {
+      if (prevDeps[idx].length === 0) {
+        clear?.(); // 빈 배열 의존성인 경우 unMounted 될 때만 실행되어야 함
+      }
+    });
+
+    try {
+      if (!Array.isArray(deps)) throw new Error('의존성은 배열이어야 합니다.');
+
+      if (
+        prevDeps[_index] &&
+        deps &&
+        prevDeps[_index].length === 0 &&
+        deps.length === 0
+      )
+        return; // 빈 배열 의존성인 경우 onMounted 시에 1번만 실행
+
+      const isDiff =
+        deps.some((dep, idx) => dep !== prevDeps[idx]) || deps.length === 0;
+
+      if (!isDiff) return;
+
+      prevDeps[_index] = deps; // 의존성 배열 업데이트
+      const clear = callback();
+      clears[_index] = clear;
+
+      if (_index === prevDeps.length - 1) {
+        prevDepsIndex = 0;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return { useState, useEffect };
 })();
