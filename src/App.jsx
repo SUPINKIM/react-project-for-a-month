74880ae035ce576b527f7fce798b115101268120
SUPINKIM <qorgkr26@sookmyung.ac.kr>
2025-02-27 17:06:27 +0900

    feat: form 태그 사용 시 submit 핸들러 처리 가능하도록 수정(제어 컴포넌트)

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,9 @@ import TodoList from './components/todo-list';
 const App = () => {
   return (
     <>
-      {/* <Header title="안녕하세요 저는 FE 개발자 김수빈입니다." /> */}
-      {/* <Content text="이건 Content 컴포넌트입니다.">
-        <p>이건 자식 노드입니다.</p>
-      </Content> */}
       {/* <h2>간단한 useState 동작 Demo</h2>
       <Counter /> */}
-      <Count />
+      {/* <Count /> */}
 
       <h2>TODO 리스트를 만들어봅시다!</h2>
       <TodoList />
